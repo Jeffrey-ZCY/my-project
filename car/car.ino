85c9c4e264d0527ebd72a41fe0fe47c6e90fd568
Jeffrey-ZCY <45502717+Jeffrey-ZCY@users.noreply.github.com>
2019-07-06 12:48:24 +0800

    Add files via upload

---
+++ car/car.ino
@@ -0,0 +1,74 @@
+void forward()
+{
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, LOW);
+}
+
+void backward()
+{
+  digitalWrite(6, HIGH);
+  digitalWrite(5, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(9, LOW);
+}
+
+void left()
+{
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(9, LOW);
+}
+
+void right()
+{
+  digitalWrite(6, HIGH);
+  digitalWrite(5, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, LOW);
+}
+
+void stop()
+{
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+}
+
+
+void setup()
+{
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  Serial.begin(9600);
+}
+
+int a=0;
+
+void loop()
+{
+  if(Serial.available()>0)
+  {
+    a=Serial.read();
+  }
+  switch(a)
+  {
+    case 'f':
+        forward();break;
+    case 'b':
+        backward();break;
+    case 'l':
+        left();break;
+    case 'r':
+        right();break;
+    case 's':
+        stop();break;
+    default:
+        break;
+  }
+}
